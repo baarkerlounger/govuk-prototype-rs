b768a3507c0bb4f6a96388eaa3c805d804248f64
baarkerlounger <db@slothlife.xyz>
2022-11-08 23:22:26 +0000

    Refactor database url construction

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,13 @@ use std::env;
 pub fn from_env() -> Figment {
     let mut database_config = HashMap::new();
     let mut databases = HashMap::new();
+    database_config.insert("url", database_url());
+    databases.insert("govuk-prototype-rs", database_config);
+
+    Config::figment().merge(("databases", databases))
+}
+
+fn database_url() -> String {
     let host = env::var("DATABASE_HOST").expect("No DATABASE_HOST environment variable found");
     let name = env::var("DATABASE_NAME").expect("No DATABASE_NAME environment variable found");
     let port = env::var("DATABASE_PORT").expect("No DATABASE_PORT environment variable found");
@@ -13,12 +20,8 @@ pub fn from_env() -> Figment {
         env::var("DATABASE_USERNAME").expect("No DATABASE_USERNAME environment variable found");
     let password =
         env::var("DATABASE_PASSWORD").expect("No DATABASE_PASSWORD environment variable found");
-    let database_url = format!(
+    format!(
         "postgres://{}:{}@{}:{}/{}",
         username, password, host, port, name
-    );
-    database_config.insert("url", database_url);
-    databases.insert("govuk-prototype-rs", database_config);
-
-    Config::figment().merge(("databases", databases))
+    )
 }
